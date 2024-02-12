39881c8b1f752893fd899a20351237d336ab06fd
XDDoS-StevenX <79353129+XDDoS-StevenX@users.noreply.github.com>
2024-02-12 14:07:09 -0500

    Add files via upload

---
+++ Proyecto-Campus.py
@@ -0,0 +1,80 @@
+#Proyecto-Campus
+
+#1 PASO:
+
+Camper_1 = {
+
+   "«•»------------(∘)------------«•» «•»------------(∘)------------«•» «•»------------(∘)------------«•»",
+                                  "N° Identificacion:" "5553228779",
+                                       "Nombres:" "Andres Galan",
+                                    "Apellidos:" "Gomez Albarra",
+                                   "Direccion:" "Carrera 12 #154-24",
+                                     "Acudiente:" "Adam Noruega.",
+                         "Telefono de contacto:" "7788246513 / Fijo: 1324456243",
+                                        "Estado:" "Inscrito",
+                                        "Riesgo:" "Ninguno.",
+                                          "Ruta: NetCore"
+}
+
+Camper_2 = {
+
+   "«•»------------(∘)------------«•» «•»------------(∘)------------«•» «•»------------(∘)------------«•»",
+                                   "N° Identificacion:" "2005784832",
+                                      "Nombres:" "Michael Steven",
+                                      "Apellidos:" "Nieves Guerra",
+                                    "Direccion:" "Carrera 24 #11-52",
+                                     "Acudiente:" "Mikhail Torres.",
+                          "Telefono de contacto:" "6215584931 / Fijo: 20025620145",
+                                        "Estado:" "Pendiente",
+                                        "Riesgo:" "Ninguno.",
+                                         "Ruta: Pendiente."
+   }
+
+Camper_3 = {
+
+   "«•»------------(∘)------------«•» «•»------------(∘)------------«•» «•»------------(∘)------------«•»",
+                                    "N° Identificacion:" "1245624587",
+                                       "Nombres:" "Adam Minguez",
+                                      "Apellidos:" "Mingo Santos",
+                                   "Direccion:" "Carrera 11 #21-04",
+                                    "Acudiente:" "Sebastian Muriel.",
+                          "Telefono de contacto:" "10034256455. Fijo: 4085320958",
+                                          "Estado:" "Cursando",
+                                        "Riesgo:" "Condicional.",
+                                              "Ruta:" "Java", 
+}
+
+Camper_4 = {
+
+   "«•»------------(∘)------------«•» «•»------------(∘)------------«•» «•»------------(∘)------------«•»",
+                                     "N° Identificacion:" "1004523118",
+                                      "Nombres:" "Bartolome Arevalo",
+                                       "Apellidos:" "Muñoz Fuentes",
+                                    "Direccion:" "Carrera 32 #22-31",
+                                      "Acudiente:" "Cesar Climent.",
+                          "Telefono de contacto:" "10597564521. Fijo: 1324867952",
+                                           "Estado:" "Aprobado",
+                                           "Riesgo:" "Ninguno.",
+                                              "Ruta:" "NodejS",
+}
+
+Camper_5 = {
+
+   "«•»------------(∘)------------«•» «•»------------(∘)------------«•» «•»------------(∘)------------«•»",
+                                    "N° Identificacion:" "1200654320",
+                                     "Nombres:" "Mariano Canales",
+                                      "Apellidos:" "Biel Morilla",
+                                    "Direccion:" "Carrera 19 #54-55",
+                                      "Acudiente:" "Eliseo Trillo.",
+                           "Telefono de contacto:" "2265513002. Fijo: 40003215650",
+                                         "Estado:" "Expulsado",
+                                          "Riesgo:" "Ninguno.",
+                                            "Ruta:" "Ninguna."
+
+    }
+
+print(Camper_1)
+print(Camper_2)
+print(Camper_3)
+print(Camper_4)
+print(Camper_5)
